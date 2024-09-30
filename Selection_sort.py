d67c86afe3beee033c6c00c3da2903f064e66aa9
Ajinesh Pratap Singh <152050389+ajinesh703@users.noreply.github.com>
2024-09-30 18:04:20 +0530

    Create Selection_sort.py

---
+++ Selection_sort.py
@@ -0,0 +1,14 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        # Swap the found minimum element with the first element
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+# Example usage
+arr = [64, 25, 12, 22, 11]
+selection_sort(arr)
+print("Sorted array:", arr)
